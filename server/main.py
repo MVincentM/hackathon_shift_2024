85041e319c9867d39fe9a888843392ea2c63b32a
Antoine Bernard <antoine.bernard@ensilog.com>
2024-06-02 00:34:25 +0200

    Updated missing requirements.txt

--- server/main.py
+++ server/main.py
@@ -1,11 +1,16 @@
 # app.py
+
+import os
+from google.auth import compute_engine
+credentials = compute_engine.Credentials()
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
